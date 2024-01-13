5ee3a64ce2d0734a8d4ba4c572d9cf46204c6fd9
“podliyPadonak” <supercell0boombeach@gmail.com>
2024-01-13 19:01:30 +0200

    solution

---
+++ ZinkovskiyTask/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using ZinkovskiyTask;
+
+namespace FileTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("РОБОТА ЗІНКОВСЬКОГО ІВАНА, ОНЛАЙН ГРУПА");
+
+            while(true) 
+            {
+                Console.Write("\nУведіть каталог:");
+                string path = Console.ReadLine();
+                Console.Write("Уведіть шукану фразу:");
+                string srch_text = Console.ReadLine();
+
+                Console.WriteLine("\nРезультати:");
+                DateTime start_time = DateTime.Now;
+                DirectoryScanner scanner = new DirectoryScanner();
+                List<string> pathes = null;
+                try
+                {
+                    pathes = scanner.GetTextFilesWithString(new string[] { path }, srch_text);
+                }
+                catch (DirectoryNotFoundException ex)
+                {
+                    Console.WriteLine("Вказаної директорії не існує!");
+                    continue;
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    Console.WriteLine("Немає доступу до вказаної директорії!");
+                    continue;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                DateTime end_time = DateTime.Now;
+                TimeSpan time_total = end_time - start_time;
+                foreach (var item in pathes)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine($"Витрачено часу: {time_total.TotalSeconds.ToString()} секунд");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ZinkovskiyTask/TextFileScanner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ZinkovskiyTask
+{
+    internal class DirectoryScanner
+    {
+        private DirectoryInfo directoryInfo;
+
+        internal DirectoryScanner() { }
+
+        internal List<string> GetTextFilesWithString(string[] dictionaries, string searchText)
+        {
+            List<string> result = new List<string>();
+
+            foreach (string directory in dictionaries)
+            {
+                directoryInfo = new DirectoryInfo(directory);
+                var files = directoryInfo.GetFiles("*", SearchOption.AllDirectories)
+                                          .AsParallel() // Використовуйте паралельність для прискорення операцій
+                                          .Where(f => GetIsFileContainsString(f.FullName, searchText));
+
+                result.AddRange(files.Select(f => f.FullName));
+            }
+            return result;
+        }
+        internal bool GetIsFileContainsString(string path, string phrase)
+        {
+            var streamReader = new StreamReader(path);
+
+            while (streamReader.Peek() != -1)
+            {
+                string line = streamReader.ReadLine();
+                if (line.Contains(phrase, StringComparison.OrdinalIgnoreCase)) // Використовуйте ігнорування регістру для пошуку
+                {
+                    return true;
+                }
+            }
+            streamReader.Close();
+            return false;
+        }
+    }
+}
